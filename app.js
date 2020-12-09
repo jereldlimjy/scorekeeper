5eaec3321054c822464bacce12fbcaced2564216
Jereld Lim <jereldlimjy@hotmail.com>
2020-12-10 01:23:52 +0800

    scorekeeper refactored

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const p1Display = document.querySelector('#p1Display');
-const p1Button = document.querySelector('#p1Button');
-const p2Display = document.querySelector('#p2Display');
-const p2Button = document.querySelector('#p2Button');
+const p1 = {
+	score: 0,
+	display: document.querySelector('#p1Display'),
+	button: document.querySelector('#p1Button')
+}
+
+const p2 = {
+	score: 0,
+	display: document.querySelector('#p2Display'),
+	button: document.querySelector('#p2Button')
+}
+
 const reset = document.querySelector('#reset');
 const target = document.querySelector('#target');
-
-let p1Score= 0;
-let p2Score = 0;
 let isGameOver = false;
 let targetScore = parseInt(target.value);
 
-p1Button.addEventListener('click', () => {
+function updateScores(player, opponent) {
 	if (!isGameOver) {
-		p1Score++;
-		p1Display.innerText = p1Score;
-		if (p1Score === targetScore) {
-			isGameOver = true;
-			p1Display.classList.add('has-text-success');
-			p2Display.classList.add('has-text-danger');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
-		}
+		player.score++;
+		player.display.innerText = player.score;
+	} if (player.score === targetScore) {
+		isGameOver = true;
+		player.display.classList.add('has-text-success');
+		opponent.display.classList.add('has-text-danger');
+		player.button.disabled = true;
+		opponent.button.disabled = true;
 	}
+
+}
+
+p1.button.addEventListener('click', () => {
+	updateScores(p1, p2);
 })
 
-p2Button.addEventListener('click', () => {
-	if (!isGameOver) {
-		p2Score++;
-		p2Display.innerText = p2Score;
-		if (p2Score === targetScore) {
-			isGameOver = true;
-			p2Display.classList.add('has-text-success');
-			p1Display.classList.add('has-text-danger');
-			p1Button.disabled = true;
-			p2Button.disabled = true;
-		}
-	}
+p2.button.addEventListener('click', () => {
+	updateScores(p2, p1);
 })
 
 target.addEventListener('change', () => {
@@ -47,12 +46,12 @@ reset.addEventListener('click', resetGame);
 
 function resetGame() {
 	isGameOver = false;
-	p1Display.classList.remove('has-text-success', 'has-text-danger');
-	p2Display.classList.remove('has-text-success', 'has-text-danger');
-	p1Button.disabled = false;
-	p2Button.disabled = false;
-	p1Display.innerText = 0;
-	p2Display.innerText = 0;
-	p1Score = 0;
-	p2Score = 0;
+	p1.display.classList.remove('has-text-success', 'has-text-danger');
+	p2.display.classList.remove('has-text-success', 'has-text-danger');
+	p1.button.disabled = false;
+	p2.button.disabled = false;
+	p1.display.innerText = 0;
+	p2.display.innerText = 0;
+	p1.score = 0;
+	p2.score = 0;
 }
